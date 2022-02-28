bc5b9f785ed1ce9dc4468118010e977479b4f7ff
joncokisler <can.cokisler@gmail.com>
2022-02-27 23:17:47 -0500

    implemented the routing into loginpage and changed a few formatting problems

--- src/login-signup/LoginPage.js
+++ src/login-signup/LoginPage.js
@@ -4,38 +4,64 @@ import Header from '../header/Header.js';
 import './LoginPage.css';
 import { withRouter } from "react-router-dom";
 import { Navigate } from 'react-router-dom';
+import SignupPage from './SignupPage.js';
 
 class LoginPage extends React.Component {
 
-    handleSignup = () =>{
+
+    state = {
+        username: "",
+        password: "",
+        validPasswordConfirm: false,
+        signupRedirect:null
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSignup = () => {
         console.log("Signup pressed")
-        return <Navigate to="/signup"/>
+        this.setState({signupRedirect:"/signupPage"})
     }
-    loginPressed = () =>{
+    loginPressed = () => {
         console.log("Login pressed")
+        console.log(`USERNAME IS ${this.state.username} `)
     }
-    handleForgotPassword = () =>{
+    handleForgotPassword = () => {
         console.log("Forgot password pressed")
     }
+
     render() {
+        if(this.state.signupRedirect){
+            return <Navigate to={this.state.signupRedirect}></Navigate>
+        }
         return (
             <div>
-                <Header/>
+                <Header />
 
                 <div id="login-header">
                     <h3>Welcome</h3>
                 </div>
 
                 <div id="inputDiv">
-                    <input class="textbox" type="text" name="username" placeholder="Enter Username"/>
-                    <input class="textbox" type="password" name="password" placeholder="Enter Password"/>
-                    <input id="submit-button" type="submit" value="Submit" onClick={ this.loginPressed }/>
+                    <input class="textbox" type="text" name="username" onChange={this.handleInputChange} value={this.state.username} placeholder="Enter Username" />
+                    <input class="textbox" type="password" name="password" onChange={this.handleInputChange} value={this.state.password} placeholder="Enter Password" />
+                    <input id="submit-button" type="submit" value="Log In" onClick={this.loginPressed} />
                 </div>
+
 
                 <div>
                     <ul>
-                        <li><a href="#" onClick={ this.handleForgotPassword }>Forgot Password? </a></li>
-                        <li><a href="#" onClick={ this.handleSignup }>Sign Up</a></li>
+                            <li><a href="#" onClick={this.handleForgotPassword}>Forgot Password? </a></li>
+
+                        <li><a href="#" onClick={this.handleSignup}>Sign Up</a></li>
                     </ul>
                 </div>
             </div>
