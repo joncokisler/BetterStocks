d122c3db9f85de0f9350a786595a5ada681e350a
Jon Cokisler <can.cokisler@gmail.com>
2022-03-01 17:39:57 -0500

    drafts for the api calls, props, state. implemented barebones html

--- src/react-components/profile/ProfilePage.js
+++ src/react-components/profile/ProfilePage.js
@@ -8,22 +8,40 @@ import { Navigate } from 'react-router-dom';
 class ProfilePage extends React.Component {
 
     state ={
-        username:"",
-        displayName:"",
-        watchlists:"",
-        bio:"",
+        watchlist:"",
+        bio:"This is a bio",
         profilePicture:"", //profile picture is a url-based image at the moment
         contactInfo: {
             phoneNumber:null,
-            email:"",
+            email:"placeholder@email.com",
         }
     }
 
+    constructor (props) {
+        super(props);
+        //do api calls to assu=ign values to states
+
+        //this.setState()
+
+    }
+    //THE COMPONENTS WILL RELY ON API CALLS TO THE SERVER TO FILL
+    // IN THE DATA
     render() {
         return (
-            <div>
-                <Header/>
+            <div id="profile-page">
+                <div id="contact-info">
+                    <h2 id="displayName">{ this.props.displayName }</h2>
+                    <h2 id="userName">{ this.props.username }</h2>
+                    <p id="bio">{ this.state.bio }</p>
+                    <p id="phone-number">{ this.state.phoneNumber }</p>
+                    <a id="change-password">Change Password</a>
+                </div>
 
+                <div id="watchlist-section">
+                    <h2>My Watchlist</h2>
+
+                </div>
+                
             </div>
         )
     }
