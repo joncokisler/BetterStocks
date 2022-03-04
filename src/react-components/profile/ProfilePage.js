0fbae6705f80650f86e251aa26dd506fde6657a9
joncokisler <can.cokisler@gmail.com>
2022-03-04 18:18:51 -0500

    overall layout of profile page with css grid

--- src/react-components/profile/ProfilePage.js
+++ src/react-components/profile/ProfilePage.js
@@ -8,6 +8,10 @@ import { Navigate } from "react-router-dom";
 
 class ProfilePage extends React.Component {
   state = {
+    //no need for these states, will be props when backend implemented
+    displayName: "",
+    userName: "",
+    //
     watchlist: "",
     bio: "",
     profilePicture: "", //profile picture is a url-based image at the moment
@@ -23,9 +27,13 @@ class ProfilePage extends React.Component {
     if (props.isAdmin === "admin") {
       this.state = {
         watchlist: "",
+        //no need for these states, will be props when backend implemented
+        displayName: "John Doe",
+        userName: "johndoe",
+        //
         bio: "I am an admin. I have powers.",
         profilePicture:
-          "https://buildyourspechere.com/wp-content/uploads/2020/10/placeholder-image-person-jpg.jpg", //profile picture is a url-based image at the moment
+          "https://image.shutterstock.com/mosaic_250/2797510/1617540484/stock-photo-closeup-photo-of-amazing-short-hairdo-lady-looking-up-empty-space-deep-thinking-creative-person-arm-1617540484.jpg", //profile picture is a url-based image at the moment
         contactInfo: {
           phoneNumber: 649 - 273 - 7381,
           email: "admin@email.com",
@@ -35,9 +43,13 @@ class ProfilePage extends React.Component {
     } else {
       this.state = {
         watchlist: "",
+        //no need for these states, will be props when backend implemented
+        displayName: "John Admin",
+        userName: "adminjoe",
+        //
         bio: "This is a bio. This website is great.",
         profilePicture:
-          "https://buildyourspechere.com/wp-content/uploads/2020/10/placeholder-image-person-jpg.jpg", //profile picture is a url-based image at the moment
+          "https://st.depositphotos.com/2309453/3449/i/600/depositphotos_34490345-stock-photo-confident-casual-unshaven-young-man.jpg",
         contactInfo: {
           phoneNumber: 646 - 548 - 2904,
           email: "johndoe@email.com",
@@ -62,18 +74,33 @@ class ProfilePage extends React.Component {
     return (
       <div>
         <Navbar />
-        
+
         <div id="profile-page">
           <div id="contact-info">
-            <h2 id="displayName">{this.props.displayName}</h2>
-            <h2 id="userName">{this.props.username}</h2>
-            <p id="bio">{this.state.bio}</p>
-            <p id="phone-number">{this.state.phoneNumber}</p>
-            <a id="change-password">Change Password</a>
-          </div>
-
-          <div id="watchlist-section">
-            <h2>My Watchlist</h2>
+            <h2 className="grid-element" id="display-name">
+              {this.state.displayName}
+            </h2>
+            <img
+              id="profile-picture"
+              className="grid-element"
+              src={this.state.profilePicture}
+              alt="Profile"
+            />
+            <h2 className="grid-element" id="user-name">
+              {this.state.username}
+            </h2>
+            <p className="grid-element" id="bio">
+              {this.state.bio}
+            </p>
+            <p className="grid-element" id="phone-number">
+              {this.state.phoneNumber}
+            </p>
+            <a className="grid-element" id="change-password">
+              Change Password
+            </a>
+            <div id="watchlist-section">
+              <h2>My Watchlist</h2>
+            </div>
           </div>
         </div>
       </div>
