028570aad83e448a559271c65a49a53562f21643
joncokisler <can.cokisler@gmail.com>
2022-03-04 16:27:26 -0500

    added navbar to use in the profile component

---
+++ src/react-components/navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Navbar.css'
+import { Navigate, NavLink } from 'react-router-dom';
+
+
+class Navbar extends React.Component {
+
+    state = {
+        profileRedirect:null,
+        topstocksRedirect:null,
+        searchRedirect:null,
+        trendingRedirect:null
+    };
+
+    render() {
+
+        return (
+            <div className="navbar sticky">
+                <div id='logo'>
+                    <a>BetterStocks</a>
+                </div>
+
+                <div id="nav-section">
+                    <ul>
+                        <li><NavLink className="nav-items" to="/top-stocks">Top Stocks</NavLink></li>
+                        <li><NavLink className="nav-items" to="/trending-stocks">Trending</NavLink></li>
+                        <li><NavLink className="nav-items" to="/search-page">Search</NavLink></li>
+                    </ul>
+
+                    <div id="profile-section">
+                        <h3>Profile</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Navbar
