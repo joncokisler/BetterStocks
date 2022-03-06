b9c5b777d347b4975695c89ad74db45dd1c120e8
joncokisler <can.cokisler@gmail.com>
2022-03-06 12:09:23 -0500

    fixed usage of 'class' vs 'className'

--- src/react-components/login-signup/LoginPage.js
+++ src/react-components/login-signup/LoginPage.js
@@ -47,8 +47,8 @@ class LoginPage extends React.Component {
                 <Header />
 
                 <div id="inputDiv">
-                    <input class="textbox" type="text" name="username" onChange={this.handleInputChange} value={this.state.username} placeholder="Enter Username" />
-                    <input class="textbox" type="password" name="password" onChange={this.handleInputChange} value={this.state.password} placeholder="Enter Password" />
+                    <input className="textbox" type="text" name="username" onChange={this.handleInputChange} value={this.state.username} placeholder="Enter Username" />
+                    <input className="textbox" type="password" name="password" onChange={this.handleInputChange} value={this.state.password} placeholder="Enter Password" />
                     <input id="submit-button" type="submit" value="Log In" onClick={this.loginPressed} />
                 
 
