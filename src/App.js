23732aa9228cc731025c2723ea5b87725a07f46c
joncokisler <can.cokisler@gmail.com>
2022-03-06 12:56:47 -0500

    moved the user data json into App.js

--- src/App.js
+++ src/App.js
@@ -10,16 +10,38 @@ import TrendingStocks from './react-components/TrendingStocks';
 
 class App extends React.Component {
 
+  users = {
+    admin: {
+      "userName": "admin",
+      "displayName": "John(Admin)",
+      "profilePicture": "",
+      "bio": "I am an admin. I have powers",
+      "email": "admin@email.com",
+      "phoneNumber": 6492737381,
+      "isAdmin": true
+    },
+    user: {
+      "userName": "user",
+      "displayName": "Fred(User)",
+      "profilePicture": "",
+      "bio": "This is a bio. This website is great.",
+      "email": "user@email.com",
+      "phoneNumber": 6482453443,
+      "isAdmin": false
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={ <LoginPage /> } />
-            <Route path='signup' element={ <SignupPage /> } />
-            <Route path='top-stocks' element={ <TopStocks /> } />
-            <Route path='trending-stocks' element={ <TrendingStocks /> } />
-            <Route path='paper-trade' element={ <PaperTrade /> } />
+            <Route path='/' element={<LoginPage />} />
+            <Route path='signup' element={<SignupPage />} />
+            <Route path='top-stocks' element={<TopStocks />} />
+            <Route path='trending-stocks' element={<TrendingStocks />} />
+            <Route path='paper-trade' element={<PaperTrade />} />
           </Routes>
         </BrowserRouter>
       </div>
