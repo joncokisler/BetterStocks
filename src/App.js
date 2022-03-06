c47b45a0acdeed0ea4bd25fa0f02fd677e9f5be3
joncokisler <can.cokisler@gmail.com>
2022-03-06 15:49:16 -0500

    changed appjs

--- src/App.js
+++ src/App.js
@@ -11,6 +11,29 @@ import Profile from './react-components/profile/ProfilePage.js'
 
 class App extends React.Component {
 
+  users = {
+    admin: {
+      userName: "admin",
+      displayName: "John(Admin)",
+      profilePicture: "",
+      bio: "I am an admin. I have powers",
+      email: "admin@email.com",
+      phoneNumber: 6492737381,
+      isAdmin: true
+    },
+    user: {
+      userName: "user",
+      displayName: "Fred(User)",
+      profilePicture: "",
+      bio: "This is a bio. This website is great.",
+      email: "user@email.com",
+      phoneNumber: 6482453443,
+      isAdmin: false,
+      watchlist:[]
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,7 +43,7 @@ class App extends React.Component {
             <Route path='/signup' element={ <SignupPage /> } />
             {/* <Route path='/top-stocks' element={ <TopStocks /> } /> */}
             {/* <Route path='/trending-stocks' element={ <TrendingStocks /> } /> */}
-            <Route path='/profile' element={ <Profile />} />
+            <Route path='/profile' element={ <Profile loggedInUser = {this.users.user} />} />
           </Routes>
         </BrowserRouter>
       </div>
