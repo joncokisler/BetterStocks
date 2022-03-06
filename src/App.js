c3361c194924cb65b444012c588ab7b891f1e85b
joncokisler <can.cokisler@gmail.com>
2022-03-06 14:52:43 -0500

    added login and signup functionality to app

--- src/App.js
+++ src/App.js
@@ -10,16 +10,65 @@ import TrendingStocks from './react-components/TrendingStocks';
 
 class App extends React.Component {
 
+  state = {
+    loggedInUser:"",
+    loginRedirect: false
+  }
+
+  users = {
+    admin: {
+      "userName": "admin",
+      "displayName": "John(Admin)",
+      "profilePicture": "",
+      "bio": "I am an admin. I have powers",
+      "email": "admin@email.com",
+      "phoneNumber": 6492737381,
+      "isAdmin": true
+    },
+    user: {
+      "userName": "user",
+      "displayName": "Fred(User)",
+      "profilePicture": "",
+      "bio": "This is a bio. This website is great.",
+      "email": "user@email.com",
+      "phoneNumber": 6482453443,
+      "isAdmin": false
+
+    }
+  }
+
+  handleLoginCallback = (childData) => {
+    if(childData.userName === "admin" && childData.password ==="admin"){
+      this.setState({loggedInUser: users.admin})
+    }
+    else if(childData.userName === "user" && childData.password ==="user"){
+      this.setState({loggedInUser: users.user})
+    }
+    else{
+      console.log("WRONG USER INFO ENTERED")
+      this.handleLoginRedirect()
+    }
+  }
+
+  handleLoginRedirect = () =>{
+    this.setState({loginRedirect:true})
+  }
+
+
   render() {
+    if(this.setState.loginRedirect){
+      this.setState({loginRedirect:false})
+      return <Navigate to={"/login"} />
+    }
     return (
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={ <LoginPage /> } />
-            <Route path='signup' element={ <SignupPage /> } />
-            <Route path='top-stocks' element={ <TopStocks /> } />
-            <Route path='trending-stocks' element={ <TrendingStocks /> } />
-            <Route path='paper-trade' element={ <PaperTrade /> } />
+            <Route path='/' element={<LoginPage />} />
+            <Route path='signup' element={<SignupPage />} />
+            <Route path='top-stocks' loggedInUser = {this.state.loggedInUser} element={<TopStocks />} />
+            <Route path='trending-stocks' loggedInUser = {this.state.loggedInUser} element={<TrendingStocks />} />
+            <Route path='paper-trade' loggedInUser = {this.state.loggedInUser} element={<PaperTrade />} />
           </Routes>
         </BrowserRouter>
       </div>
