d8c45e088cf1d11f1cf81e8e1d7635b818734baf
Raymond Li <34190736+raylfli@users.noreply.github.com>
2022-03-05 21:32:40 -0500

    Add reviews button to stock-page

--- src/react-components/stock-trend/index.js
+++ src/react-components/stock-trend/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, NavLink } from 'react-router-dom';
 
 import './styles.css';
 
 function Stock() {
     let [params, setParams] = useSearchParams();
 
-    return <p>{ params.get('symbol') }</p>
+    
+
+    return (
+        <div className='stockPage'>
+            <p>{ params.get('symbol') }</p>
+            <NavLink to={ `reviews?symbol=${params.get('symbol')}` }>Reviews</NavLink>
+        </div>
+    );
 }
 
 export default Stock;
