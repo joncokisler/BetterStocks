f099821f249cdf8f3623e119ed21eaddad47c877
Raymond Li <34190736+raylfli@users.noreply.github.com>
2022-03-28 19:34:14 -0400

    Remove redundant code in stocks routes

--- routes/stocks.js
+++ routes/stocks.js
@@ -6,11 +6,6 @@ const { mongoChecker, isMongoError } = require("./helpers/mongo_helpers");
 const { authenticate, adminAuthenticate } = require("./helpers/authentication");
 
 const { Stock } = require('../models/stock');
-const { default: isHalfWidth } = require('validator/lib/isHalfWidth');
-
-const env = process.env.NODE_ENV;
-const USE_TEST_USER = env !== 'production' && process.env.TEST_USER_ON; // option to turn on the test user.
-const TEST_USER_ID = '623e007952cdf8af877cef4c' // the id of our test user (you will have to replace it with a test user that you made). can also put this into a separate configutation file
 
 /***************** STOCK CRUD **************************/
 
