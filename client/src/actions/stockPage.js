ac1b369475442f7978a5305064f505ce55f0509e
Raymond Li <34190736+raylfli@users.noreply.github.com>
2022-04-05 19:40:14 -0400

    Add add to watchlist button

--- client/src/actions/stockPage.js
+++ client/src/actions/stockPage.js
@@ -14,3 +14,23 @@ export async function getStockInfo(stock, setStockInfo) {
         setStockInfo(resJSON[0]);
     }
 }
+
+export async function addToWatchlist(stock, setErrorMessage) {
+    try {
+        const url = `${API_HOST}/api/users/watchlist`;
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    stock: stock
+                })
+        });
+        if (res.status !== 200) {
+            setErrorMessage('Invalid stock!');
+        } else {
+            setErrorMessage('');
+        }
+    } catch (error) {
+        setErrorMessage('An unknown error occurred.');
+    }
+}
